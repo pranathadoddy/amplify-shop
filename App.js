9532f61862fc3d9dedc0d099dc1653fb97af5e71
TDSLaptop\tds <mda.pranatha@tds-group.com>
2022-08-21 14:46:40 +0800

    Init project

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Amplify } from "aws-amplify";
+import awsconfig from './aws-exports';
+
+Amplify.configure({
+  ...awsconfig,
+  Analytics: {
+    disabled: true,
+  },
+});
+
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
